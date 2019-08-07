3734235ea7d3f6ad37666ec2485d80f5b3282763
Iqra-Nazeer <52270717+Iqra-Nazeer@users.noreply.github.com>
2019-08-07 20:28:01 +0500

    first commit
    nothing

---
+++ products.php
@@ -0,0 +1,13 @@
+<form method = "post" action ="<?php echo site_url('welcome/add_products');?>">
+    Products:
+    <input type="text" name="name">
+   
+  
+    Price
+    <input type="text" name="price">
+    Descp
+    <input type="text" name="descp">
+    <!--<input type="file" name="img">-->
+    <input type="submit" name="submit">
+
+</form>
